54f68632e8ea6a17758fe1e9a9ea0bec0cb1baed
COGNOSPHERE PTE. LTD <beta.taster.yoi@gmail.com>
2024-10-11 14:39:42 +0700

    Update 鸣潮 Beta.bat

--- 鸣潮 Beta.bat
+++ 鸣潮 Beta.bat
@@ -1,9 +1,9 @@
-// Wuthering Waves 1.3 Beta
+// Wuthering Waves 1.4 Beta
 // Launcher
-start https://pcdownload-huoshan.aki-game.com/pcstarter/prod/starter/10008_Pa0Q0EMFxukjEqX33pF9Uyvdc8MaGPSz/G152/1.6.1.0/lrXV5DtqdqnCMFAqMctsXWmZyjLeZxHF/installer.exe
+start https://pcdownload-huoshan.aki-game.com/pcstarter/prod/starter/10008_Pa0Q0EMFxukjEqX33pF9Uyvdc8MaGPSz/G152/1.6.4.0/DzShblKI5z3ylF1eRFo6yTgnvnZiJVLa/installer.exe
 // Launcher.json
 start https://prod-cn-alicdn-gamestarter.kurogame.com/pcstarter/prod/starter/10008_Pa0Q0EMFxukjEqX33pF9Uyvdc8MaGPSz/G152/index.json
 // index.json
 start https://prod-cn-alicdn-gamestarter.kurogame.com/pcstarter/prod/game/G152/10008_Pa0Q0EMFxukjEqX33pF9Uyvdc8MaGPSz/index.json
 // resource.json
-start https://pcdownload-wangsu.aki-game.com/pcstarter/prod/game/G152/1.3.0/JNITFNSeJqvnKYvrplg3gVR71FTq6x01/resource.json
+start https://pcdownload-huoshan.aki-game.com/pcstarter/prod/game/G152/1.4.0/iQPhVvIx0vVjUoykmdLNMSHN0sfNWeij/resource.json
