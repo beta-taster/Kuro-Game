d62b8a2255dd6a951f15bb3039e76573bb49a25d
COGNOSPHERE PTE. LTD <beta.taster.yoi@gmail.com>
2024-10-18 17:55:06 +0700

    Delete 鸣潮 Beta.bat

--- 鸣潮 Beta.bat
+++
@@ -1,9 +0,0 @@
-// Wuthering Waves 1.4 Beta
-// Launcher
-start https://pcdownload-huoshan.aki-game.com/pcstarter/prod/starter/10008_Pa0Q0EMFxukjEqX33pF9Uyvdc8MaGPSz/G152/1.6.4.0/DzShblKI5z3ylF1eRFo6yTgnvnZiJVLa/installer.exe
-// Launcher.json
-start https://prod-cn-alicdn-gamestarter.kurogame.com/pcstarter/prod/starter/10008_Pa0Q0EMFxukjEqX33pF9Uyvdc8MaGPSz/G152/index.json
-// index.json
-start https://prod-cn-alicdn-gamestarter.kurogame.com/pcstarter/prod/game/G152/10008_Pa0Q0EMFxukjEqX33pF9Uyvdc8MaGPSz/index.json
-// resource.json
-start https://pcdownload-huoshan.aki-game.com/pcstarter/prod/game/G152/1.4.0/iQPhVvIx0vVjUoykmdLNMSHN0sfNWeij/resource.json
