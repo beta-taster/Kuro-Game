b55f97fa5f8f1d951029b00b9283dba5ee6df4e2
COGNOSPHERE PTE. LTD <beta.taster.yoi@gmail.com>
2024-10-18 17:54:55 +0700

    Delete OS  Live.bat

--- OS  Live.bat
+++
@@ -1,2 +0,0 @@
-start https://prod-volcdn-gamestarter.kurogame.net/pcstarter/prod/starter/50004_obOHXFrFanqsaIEOmuKroCcbZkQRBC7c/G153/index.json
-start https://prod-alicdn-gamestarter.kurogame.com/pcstarter/prod/game/G153/50004_obOHXFrFanqsaIEOmuKroCcbZkQRBC7c/index.json
